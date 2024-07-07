26139a99b5922c819d6649dc8e344be5617f5a8a
nmnhi <minhnhi08061105@gmail.com>
2024-07-07 21:43:48 +0700

    config font

---
+++ src/constants/fonrFamilies.ts
@@ -0,0 +1,12 @@
+export const fontFamilies = {
+  poppinsLight: 'Poppins-Light',
+  poppinsMedium: 'Poppins-Medium',
+  poppinsRegular: 'Poppins-Regular',
+  poppinsSemiBold: 'Poppins-SemiBold',
+  poppinsBold: 'Poppins-Bold',
+  robotoLight: 'Roboto-Light',
+  robotoMedium: 'Roboto-Medium',
+  robotoRegular: 'Roboto-Regular',
+  robotoSemiBold: 'Roboto-SemiBold',
+  robotoBold: 'Roboto-Bold',
+};
